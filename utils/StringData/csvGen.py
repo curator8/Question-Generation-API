fb80b99b49de4d15146f23a5459efa0df9ba9626
Trevor Strobel <trevorstrobel@gmail.com>
2021-05-19 19:49:13 -0400

    moved the csv gen files within the 'StringData' folder

--- utils/StringData/csvGen.py
+++ utils/StringData/csvGen.py
@@ -5,33 +5,33 @@ import csv
 from salarStrings import country_names, city_names, male_names, female_names
    
 # All Data
-with open('StringData/1.csv', mode='w') as csvFile:
+with open('1.csv', mode='w') as csvFile:
    data = country_names + city_names +  male_names + female_names
    for x in data:
        csvFile.write(x + "\n")
 
 # Countries
-with open('StringData/2.csv', mode='w') as csvFile:
+with open('2.csv', mode='w') as csvFile:
     for x in country_names:
         csvFile.write(x +"\n")
 
 # Cities
-with open('StringData/3.csv', mode='w') as csvFile:
+with open('3.csv', mode='w') as csvFile:
     for x in city_names:
         csvFile.write(x +"\n")        
 
 # Male Names
-with open('StringData/4.csv', mode='w') as csvFile:
+with open('4.csv', mode='w') as csvFile:
     for x in male_names:
         csvFile.write(x +"\n")
 
 # Female Names
-with open('StringData/5.csv', mode='w') as csvFile:
+with open('5.csv', mode='w') as csvFile:
     for x in female_names:
         csvFile.write(x +"\n")
 
 # All Names
-with open('StringData/6.csv', mode='w') as csvFile:
+with open('6.csv', mode='w') as csvFile:
    names = male_names + female_names
    for x in names:
        csvFile.write(x + "\n")
